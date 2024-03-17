3429ac1d46ac6e72733ec092b6747a2ea5d2dd94
Landi <lizanderbarjamaj@yahoo.com>
2024-03-17 16:45:15 +0100

    C

--- config/server.js
+++ config/server.js
@@ -7,4 +7,8 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  session: {
+    secret: env('SESSION_SECRET'),
+    keys: ['Landi1', 'Landi2'], 
+  },
 });
