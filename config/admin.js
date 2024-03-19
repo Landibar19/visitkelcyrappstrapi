120b1eae01c41d4e91093cfc5dd7754d2abaf39a
Landi <lizanderbarjamaj@yahoo.com>
2024-03-19 21:41:40 +0100

    'l'

--- config/admin.js
+++ config/admin.js
@@ -3,11 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', 'nNTx4HA7gV2ecP2WF5ZT9g=='),
   },
   apiToken: {
-<<<<<<< HEAD
     salt: env('API_TOKEN_SALT', '7WVUAKj60x4S8jNte+plmg=='),
-=======
-    salt: env('API_TOKEN_SALT', "q0BQu+8ARJOaw4m5CaOdwg=="),
->>>>>>> 4025954871faeec7520438c90864203025b334a5
   },
   transfer: {
     token: {
