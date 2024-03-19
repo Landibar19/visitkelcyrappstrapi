248ac74eb4df86331dda1c0235e90d05448edb5d
Landi <lizanderbarjamaj@yahoo.com>
2024-03-19 21:50:45 +0100

    'l'

--- getsecret/getsecret.js
+++ getsecret/getsecret.js
@@ -1,10 +1,3 @@
 const crypto = require('crypto');
-<<<<<<< HEAD
 const secret = crypto.randomBytes(16).toString('base64');
 console.log(secret);
-=======
-
-const salt = crypto.randomBytes(16).toString('base64');
-
-console.log(salt);
->>>>>>> 4025954871faeec7520438c90864203025b334a5
